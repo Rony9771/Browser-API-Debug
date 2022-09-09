69575f588317ccaf0659897bafa7235e96d11a62
Rony <rony.ahmed9771@gmail.com>
2022-09-09 16:40:01 +0600

    confirm added

--- js/prompt.js
+++ js/prompt.js
@@ -10,3 +10,13 @@ const showAlert = () =>{
   }
   
 }
+const showConfirm = () =>{
+  const decision=confirm ('How Are You?')
+
+  if (decision ===true){
+    alert ("I'm fine")
+  }
+  else{
+    console.log('Not so good')
+  }
+}
